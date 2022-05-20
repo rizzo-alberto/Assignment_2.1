7bc49d2460a0b984160bb76c4b3ee4f9d4ea0ec3
sviluppatore1 <rizzo.alberto.000@gmail.com>
2022-05-20 21:37:44 +0200

    Creati file di test

---
+++ src/test/java/it/unipd/mtss/business/BillTest.java
@@ -0,0 +1,7 @@
+package it.unipd.mtss.business;
+
+import static org.junit.Assert.*;
+
+public class BillTest {
+
+}
---
+++ src/test/java/it/unipd/mtss/model/EItemTest.java
@@ -0,0 +1,7 @@
+package it.unipd.mtss.model;
+
+import static org.junit.Assert.*;
+
+public class EItemTest {
+
+}
