56f703e30f6799b5b5d00a5e0d6b8f9cab60689a
sviluppatore1 <rizzo.alberto.000@gmail.com>
2022-05-21 17:09:20 +0200

    Aggiunti test base alla classe Bill

--- src/main/java/it/unipd/mtss/business/BillImplementation.java
+++ src/main/java/it/unipd/mtss/business/BillImplementation.java
@@ -14,6 +14,24 @@ import java.util.List;
 public class BillImplementation implements Bill {
     public double getOrderPrice(List<EItem> itemsOrdered, User user) throws BillException {
         double tot = 0.0;
+
+        if(user == null) {
+            throw new BillException("Il cliente deve essere definito");
+        }
+
+        if(itemsOrdered.contains(null)) {
+            throw new BillException("I prodotti nella lista non possono essere nulli");
+        }
+
+        if(itemsOrdered == null) {
+            throw new BillException("La lista degli ordini non può essere nulla");
+        }
+
+        if(itemsOrdered.isEmpty()) {
+            throw new BillException("La lista degli ordini non può essere vuota");
+        }
+
+
         return tot;
     }
 }
--- src/test/java/it/unipd/mtss/business/BillTest.java
+++ src/test/java/it/unipd/mtss/business/BillTest.java
@@ -1,7 +1,62 @@
+////////////////////////////////////////////////////////////////////
+// Alberto Rizzo 2008079
+// Alberto Franzin 2011879
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss.business;
+
+import it.unipd.mtss.business.exception.BillException;
+import it.unipd.mtss.model.EItem;
+import it.unipd.mtss.model.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class BillTest {
+    Bill bill = null;
+    List<EItem> orderList = null;
+    User usr = null;
+    @Before
+    public void beforeTests() {
+        bill = new BillImplementation();
+        orderList = new ArrayList<EItem>();
+        usr = new User("The","Roccia", LocalDate.of(1961,5,11),"elonmaschera@fatturare.soldi.it");
+    }
 
+    @Test
+    public void orderPrice_NullUserTest(){
+        orderList.add(new EItem(EItem.itemEnum.Mouse, "Trust mePlease_black", 9.99));
+        try {
+            bill.getOrderPrice(orderList,null);
+        }
+        catch(BillException e){
+            assertEquals("Il cliente deve essere definito",e.getMessage());
+        }
+    }
+
+    @Test
+    public void orderPrice_NullEItemTest(){
+        orderList.add(null);
+        try {
+            bill.getOrderPrice(orderList,usr);
+        }
+        catch(BillException e){
+            assertEquals("I prodotti nella lista non possono essere nulli",e.getMessage());
+        }
+    }
+
+    @Test
+    public void orderPrice_EmptyList(){
+        try {
+            bill.getOrderPrice(orderList,usr);
+        }
+        catch(BillException e){
+            assertEquals("La lista degli ordini non può essere vuota",e.getMessage());
+        }
+    }
 }
