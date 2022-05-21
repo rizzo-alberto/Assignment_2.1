ca9bef660ea6c2cc8c8a51c0abfd7d181d7b4506
sviluppatore2 <alberto.franzin.1@studenti.unipd.it>
2022-05-21 15:09:59 +0200

    fix jacoco plugin, sviluppo delle classi Eitem e User

--- src/main/java/it/unipd/mtss/model/EItem.java
+++ src/main/java/it/unipd/mtss/model/EItem.java
@@ -6,4 +6,26 @@
 package it.unipd.mtss.model;
 
 public class EItem {
+    public enum itemEnum{Processor, Motherboard, Mouse, Keyboard};
+    private String name;
+    private double price;
+    private itemEnum itemType;
+
+    public EItem(itemEnum itemType, String name, double price){
+        if (price <= 0){
+            throw new IllegalArgumentException("Il prezzo deve essere positivo e non nullo");
+        }
+        this.price = price;
+        this.name = name;
+        this.itemType = itemType;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public itemEnum getItemType(){
+        return itemType;
+    }
 }
--- src/main/java/it/unipd/mtss/model/User.java
+++ src/main/java/it/unipd/mtss/model/User.java
@@ -5,5 +5,46 @@
 
 package it.unipd.mtss.model;
 
+import java.time.LocalDate;
+
 public class User {
+    private String name;
+    private String surname;
+    private LocalDate birth;
+    private String email;
+
+    public  User(String name, String surname, LocalDate birth, String email){
+        if(email.trim() == ""){
+            throw new IllegalArgumentException("L'email non può essere vuota!");
+        }
+        if(name.trim() == ""){
+            throw new IllegalArgumentException("Il nome non può essere vuoto!");
+        }
+        if(surname.trim() == ""){
+            throw new IllegalArgumentException("Il cognome non può essere vuoto!");
+        }
+        if(birth == null){
+            throw new IllegalArgumentException("Bisogna specificare la data di nascia!");
+        }
+        if(birth.isAfter(LocalDate.now())){
+            throw new IllegalArgumentException("La data di nascita non può essere nel futuro!");
+        }
+        this.name = name;
+        this.surname = surname;
+        this.birth = birth;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public LocalDate getBirth(){
+        return birth;
+    }
+    public String getEmail(){
+        return email;
+    }
 }
